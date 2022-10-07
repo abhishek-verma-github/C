113cbf9f11589354b240269bd7c264a2b0dfdfaa
Abhishek Verma <59361086+abhishek-verma-github@users.noreply.github.com>
2022-10-07 17:45:06 +0530

    directory refactored

---
+++ AlgorithmProblems/two_dim_peakfinder.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* getinput(char* carr){
+    //todo...
+}
+
+int * findpeak(int* inp){
+    //todo...
+}
+
+int main(int argc, char * argv[]){
+    // todo...
+
+    return EXIT_SUCCESS;
+}
